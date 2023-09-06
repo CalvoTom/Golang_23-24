a31436f0afd18ad3580a0b993fa3327b68b295a9
catom <tom.calvo@ynov.com>
2023-09-06 17:13:05 +0200

    Piscine pointcomb V.1

---
+++ printcomb.go
@@ -0,0 +1,24 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for i := 48; i < 58; i++ {
+		r1 := rune(i)
+		for j := 48; j < 58; j++ {
+			r2 := rune(j)
+			for k := 48; k < 58; k++ {
+				r3 := rune(k)
+				if r1 < r2 && r2 < r3 {
+					z01.PrintRune(r1)
+					z01.PrintRune(r2)
+					z01.PrintRune(r3)
+					z01.PrintRune(rune(44))
+					z01.PrintRune(rune(32))
+				}
+
+			}
+		}
+	}
+	z01.PrintRune(rune('\n'))
+}
