98a314ae90cba443ad09f0a44474d57e1c081c8c
catom <tom.calvo@ynov.com>
2023-09-07 16:16:37 +0200

    Piscine Quest 2 AlphaCount V.1

---
+++ alphacount.go
@@ -0,0 +1,12 @@
+package piscine
+
+func AlphaCount(s string) int {
+	str_rn := []rune(s)
+	counter := 0
+	for i := 0; i < len(str_rn); i++ {
+		if str_rn[i] < 'z' && str_rn[i] > 'a' || str_rn[i] < 'Z' && str_rn[i] > 'A' {
+			counter += 1
+		}
+	}
+	return counter
+}
