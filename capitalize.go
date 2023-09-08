4601e49d7fb79007df5842e0525553981bf7969d
catom <tom.calvo@ynov.com>
2023-09-08 12:12:36 +0200

    Piscine Quest 3 capitalize V.2

--- capitalize.go
+++ capitalize.go
@@ -2,13 +2,23 @@ package piscine
 
 func Capitalize(s string) string {
 	str_rn := []rune(s)
+	need_maj := 1
+	if str_rn[0] <= 90 && str_rn[0] >= 65 {
+		need_maj = 0
+	}
 	for i := 1; i < len(str_rn); i++ {
-		if str_rn[i] <= 90 && str_rn[i] >= 65 {
+		if str_rn[i] <= 90 && str_rn[i] >= 65 && need_maj == 0 {
 			str_rn[i] = str_rn[i] + 32
 		}
-		if !(str_rn[i] == '.' || str_rn[i] == '!' || str_rn[i] == '?') {
-			i += 1
-			continue
+		if str_rn[i] <= 122 && str_rn[i] >= 97 && need_maj == 1 {
+			str_rn[i] = str_rn[i] - 32
+			need_maj = 0
+		}
+		if str_rn[i] <= 90 && str_rn[i] >= 65 && need_maj == 1 {
+			need_maj = 0
+		}
+		if str_rn[i] == '.' || str_rn[i] == '!' || str_rn[i] == '?' {
+			need_maj = 1
 		}
 	}
 	return string(str_rn)
