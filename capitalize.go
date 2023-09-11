ea03d9f702befb96b862d95559de34e815257b73
catom <tom.calvo@ynov.com>
2023-09-11 15:03:34 +0200

    Piscine Quest 3 capitalize V.11

--- capitalize.go
+++ capitalize.go
@@ -2,23 +2,32 @@ package piscine
 
 func Capitalize(s string) string {
 	str_rn := []rune(s)
-	need_maj := 1
-	if str_rn[0] <= 90 && str_rn[0] >= 65 {
-		need_maj = 0
+	if str_rn[0] >= 'a' && str_rn[0] <= 'z' {
+		str_rn[0] = str_rn[0] - 32
 	}
 	for i := 1; i < len(str_rn); i++ {
-		if str_rn[i] <= 90 && str_rn[i] >= 65 && need_maj == 0 {
-			str_rn[i] = str_rn[i] + 32
+		if str_rn[i] >= 'A' && str_rn[i] <= 'Z' {
+			if str_rn[i-1] >= 'a' && str_rn[i-1] <= 'z' {
+				str_rn[i] = str_rn[i] + 32
+			}
+			if str_rn[i-1] >= 'A' && str_rn[i-1] <= 'Z' {
+				str_rn[i] = str_rn[i] + 32
+			}
+			if str_rn[i-1] >= '0' && str_rn[i-1] <= '9' {
+				str_rn[i] = str_rn[i] + 32
+			}
 		}
-		if str_rn[i] <= 122 && str_rn[i] >= 97 && need_maj == 1 {
+		if str_rn[i] >= 'a' && str_rn[i] <= 'z' {
+			if str_rn[i-1] >= 'a' && str_rn[i-1] <= 'z' {
+				continue
+			}
+			if str_rn[i-1] >= 'A' && str_rn[i-1] <= 'Z' {
+				continue
+			}
+			if str_rn[i-1] >= '0' && str_rn[i-1] <= '9' {
+				continue
+			}
 			str_rn[i] = str_rn[i] - 32
-			need_maj = 0
-		}
-		if str_rn[i] <= 90 && str_rn[i] >= 65 && need_maj == 1 {
-			need_maj = 0
-		}
-		if str_rn[i-1] == '.' || str_rn[i-1] == '!' || str_rn[i-1] == '?' || str_rn[i-1] == '}' || str_rn[i-1] == '>' || str_rn[i-1] == '[' || str_rn[i-1] == ']' || str_rn[i-1] == '^' || str_rn[i-1] == '+' {
-			str_rn[i] = str_rn[i] + 32
 		}
 	}
 	return string(str_rn)
