90177a127c7969695dab154545727f01a30b0059
catom <tom.calvo@ynov.com>
2023-09-11 16:25:36 +0200

    Piscine Quest 3 trimatoi V.2

--- trimatoi.go
+++ trimatoi.go
@@ -1,15 +1,17 @@
 package piscine
 
 func TrimAtoi(s string) int {
-	nb := 0
+	var nb int
 	signe := 0
+	av := 0
 	for i := 0; i < len(s); i++ {
 		for j := 0; j <= 9; j++ {
 			if int(s[i]-48) == j {
+				av = 1
 				nb = (nb * 10) + int(s[i]-48)
 			}
 		}
-		if s[i] == '-' && nb != 0 {
+		if s[i] == '-' && av == 0 {
 			signe = 1
 		}
 	}
