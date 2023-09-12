bfe56cd13833584fa6b653705168311b29bab100
catom <tom.calvo@ynov.com>
2023-09-12 16:04:32 +0200

    Piscine Quest 4 RecursivePower V.1

--- printdigits/main.go
+++ printdigits/main.go
@@ -1,11 +1,10 @@
 package main
 
-import "github.com/01-edu/z01"
+import (
+	"fmt"
+	"piscine"
+)
 
 func main() {
-	for i := 48; i < 58; i++ {
-		rn := rune(i)
-		z01.PrintRune(rn)
-	}
-	z01.PrintRune('\n')
+	fmt.Println(piscine.RecursivePower(4, 3))
 }
