d5f90b59015dc8d185854047add5484d2417d1d5
catom <tom.calvo@ynov.com>
2023-09-12 14:46:53 +0200

    Piscine Quest 4 IterativeFactorial V.5

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,8 +1,11 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb > 10 || nb < 0 {
+	if nb > 10 || nb <= 0 {
 		return 0
+	}
+	if nb == 0 {
+		return 1
 	}
 	if nb != 1 {
 		nb = nb * IterativeFactorial(nb-1)
