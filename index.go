7bd5aa2b8195d342cc028a01b58f70b5f7d19cc5
catom <tom.calvo@ynov.com>
2023-09-12 10:21:05 +0200

    Piscine Quest 3 Index V.3

--- index.go
+++ index.go
@@ -4,7 +4,7 @@ func Index(s string, toFind string) int {
 	tab_s := []rune(s)
 	tab_tf := []rune(toFind)
 	if len(tab_tf) == 0 {
-		return -1
+		return 0
 	}
 	for i := 0; i < len(tab_s); i++ {
 		if tab_s[i] == tab_tf[0] {
