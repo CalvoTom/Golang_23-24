fdc10914a85a872bfd799bb16829de2ff92ffe33
catom <tom.calvo@ynov.com>
2023-09-12 10:16:56 +0200

    Piscine Quest 3 Index V.2

--- main/main.go
+++ main/main.go
@@ -7,6 +7,6 @@ import (
 
 func main() {
 	fmt.Println(piscine.Index("Hello!", "l"))
-	fmt.Println(piscine.Index("Salut!", "alu"))
-	fmt.Println(piscine.Index("Ola!", "hOl"))
+	fmt.Println(piscine.Index("Salut!", ""))
+	fmt.Println(piscine.Index("Ola!", ""))
 }
