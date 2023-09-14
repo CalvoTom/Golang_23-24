b107db59919df81b63cfe5032954af3c4d711e66
CalvoTom <tom.calvo@ynov.com>
2023-09-14 10:50:08 +0200

    Piscine Quest 5 point V.5

--- point/main.go
+++ point/main.go
@@ -15,20 +15,19 @@ func setPoint(ptr *point) {
 func main() {
 	points := &point{}
 	setPoint(points)
-
 	z01.PrintRune('x')
 	z01.PrintRune(' ')
 	z01.PrintRune('=')
 	z01.PrintRune(' ')
-	z01.PrintRune('4')
-	z01.PrintRune('2')
+	z01.PrintRune(4 + 48)
+	z01.PrintRune(2 + 48)
 	z01.PrintRune(',')
 	z01.PrintRune(' ')
 	z01.PrintRune('y')
 	z01.PrintRune(' ')
 	z01.PrintRune('=')
 	z01.PrintRune(' ')
-	z01.PrintRune('2')
-	z01.PrintRune('1')
+	z01.PrintRune(2 + 48)
+	z01.PrintRune(1 + 48)
 	z01.PrintRune('\n')
 }
