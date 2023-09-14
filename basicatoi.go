f0bc804e7efcf158b1a0a0fea70dcfec092bb045
CalvoTom <tom.calvo@ynov.com>
2023-09-14 11:34:44 +0200

    Piscine Quest Basicatoi V.1

---
+++ basicatoi.go
@@ -0,0 +1,11 @@
+package piscine
+
+func BasicAtoi(s string) int {
+	var nb int
+	for i := 0; i < len(s); i++ {
+		if int(s[i]-48) >= 0 && int(s[i]-48) <= 9 {
+			nb = (nb * 10) + int(s[i]-48)
+		}
+	}
+	return nb
+}
