165ae9d075ab5698fab24cd55510bbb3b1ae0f91
CalvoTom <tom.calvo@ynov.com>
2023-09-14 11:43:43 +0200

    Piscine Quest atoi V.1

---
+++ atoi.go
@@ -0,0 +1,26 @@
+package piscine
+
+func Atoi(s string) int {
+	var nb int
+	signe := 0
+	j := 0
+	if s[0] == '-' {
+		signe = 1
+		j += 1
+	}
+	if s[0] == '+' {
+		signe = 0
+		j += 1
+	}
+	for i := j; i < len(s); i++ {
+		if int(s[i]-48) >= 0 && int(s[i]-48) <= 9 {
+			nb = (nb * 10) + int(s[i]-48)
+		} else {
+			return 0
+		}
+	}
+	if signe == 1 {
+		nb = -nb
+	}
+	return nb
+}
