7ee61c94b0f0dbcda4e62ecf8d4fb2fe2a276859
CalvoTom <tom.calvo@ynov.com>
2023-09-18 12:42:14 +0200

    Piscine Quest 7 appendrange V.5

--- makerange.go
+++ makerange.go
@@ -1,16 +1,14 @@
 package piscine
 
 func MakeRange(min, max int) []int {
-	var tab []int
 	if min >= max {
-		return tab
+		return nil
 	}
-	tab = make([]int, min, max)
+	tab := make([]int, min, max)
 	size := max - min
 	for i := 0; i < size; i++ {
 		tab[i] = min
 		min += 1
-
 	}
 	return tab
 }
