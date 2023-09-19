b53690ec9ac3f3cea46ab4f5c9f930a7ff1e0dff
CalvoTom <tom.calvo@ynov.com>
2023-09-19 11:35:15 +0200

    Piscine Quest 7 printwordstables V.2

--- main/main.go
+++ main/main.go
@@ -1,8 +1,11 @@
 package main
 
-import "piscine"
+import (
+	"fmt"
+	"piscine"
+)
 
 func main() {
-	a := piscine.SplitWhiteSpaces("0;4zW0;[yh`J1;?Y F`|!P^heoA=8yC$=9`+YL&K u\\j\\*7=~jLA| X]y(?c3! w?-#z[31YPEF37MBYsMtJh`bEk#oxM\"^c]usNgL.xj<B")
-	piscine.PrintWordsTables(a)
+	s := "HelloHAhowHAareHAyou?HA"
+	fmt.Printf("%#v\n", piscine.Split(s, "HA"))
 }
