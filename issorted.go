eb76459d03fe145e21d629e83a63a9728de21b87
CalvoTom <tom.calvo@ynov.com>
2023-09-19 12:31:11 +0200

    Piscine Quest 8 issorted V.3

--- issorted.go
+++ issorted.go
@@ -2,9 +2,25 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	testeur := f(a[0], a[1])
-	for i := 1; i < len(a); i++ {
-		if f(a[i-1], a[i]) != testeur {
-			return false
+	if testeur < 0 {
+		for i := 1; i < len(a); i++ {
+			if f(a[i-1], a[i]) >= 0 {
+				return false
+			}
+		}
+	}
+	if testeur > 0 {
+		for i := 1; i < len(a); i++ {
+			if f(a[i-1], a[i]) <= 0 {
+				return false
+			}
+		}
+	}
+	if testeur == 0 {
+		for i := 1; i < len(a); i++ {
+			if f(a[i-1], a[i]) != 0 {
+				return false
+			}
 		}
 	}
 	return true
