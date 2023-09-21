3097df40feb4348048e12d3320480ba7062df8a4
CalvoTom <tom.calvo@ynov.com>
2023-09-21 15:50:40 +0200

    Piscine Quest 6 flags V.4

--- flags/main.go
+++ flags/main.go
@@ -151,12 +151,7 @@ func tabOrdering(args, array []rune) {
 }
 
 func help() {
-	fmt.Println("--insert")
-	fmt.Println("  -i")
-	fmt.Println("         This flag inserts the string into the string passed as argument.")
-	fmt.Println("--order")
-	fmt.Println("  -o")
-	fmt.Println("         This flag will behave like a boolean, if it is called it will order the argument.")
+	fmt.Println("--insert\n  -i\n\t This flag inserts the string into the string passed as argument.\n--order\n  -o\n\t This flag will behave like a boolean, if it is called it will order the argument.")
 }
 
 func PrintStr(s string) {
