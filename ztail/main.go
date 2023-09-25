f9ec7e544a659d3af73cb7093a152f4ef32ddebf
CalvoTom <tom.calvo@ynov.com>
2023-09-25 11:25:23 +0200

    Piscine Quest 9 ztail V.2

--- ztail/main.go
+++ ztail/main.go
@@ -7,20 +7,29 @@ import (
 
 func main() {
 	arguments := os.Args[1:]
+	var nb int
 	if len(arguments) == 0 {
 		fmt.Println(1)
 	}
 	if arguments[0] == "-c" && int(arguments[1][0]-48) > 0 {
+		for _, carac := range arguments[1] {
+			nb = (nb * 10) + int(carac-48)
+		}
 		for index, ch := range arguments {
 			if len(arguments) > 3 {
 				if index >= 2 {
 					data, err := os.ReadFile(ch)
 					if err != nil {
+						if index == len(arguments)-1 {
+							fmt.Println(err)
+							continue
+						}
 						fmt.Println(err)
+						fmt.Println()
 						continue
 					}
 					fmt.Println("==>", ch, "<==")
-					for i := len(data) - 3; i < len(data); i++ {
+					for i := (len(data) - nb + 1); i < len(data); i++ {
 						fmt.Printf(string(data[i]))
 					}
 					fmt.Println()
@@ -29,10 +38,16 @@ func main() {
 				if index == 2 {
 					data, err := os.ReadFile(ch)
 					if err != nil {
+						if index == len(arguments)-1 {
+							fmt.Println(err)
+							continue
+						}
 						fmt.Println(err)
+						fmt.Println()
 						continue
 					}
-					for i := len(data) - 3; i < len(data); i++ {
+					fmt.Println()
+					for i := (len(data) - nb + 1); i < len(data); i++ {
 						fmt.Printf(string(data[i]))
 					}
 					fmt.Println()
