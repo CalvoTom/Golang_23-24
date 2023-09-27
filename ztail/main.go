e1429751462e0aad7dd2ba4e2d22f2b0cd375855
CalvoTom <tom.calvo@ynov.com>
2023-09-27 12:10:05 +0200

    Piscine Quest 9 ztail V.1.1

--- ztail/main.go
+++ ztail/main.go
@@ -6,81 +6,67 @@ import (
 )
 
 func main() {
-	arguments := os.Args[1:]
-	var nb int
-	if len(arguments) == 0 {
-		fmt.Println(1)
-	}
-	if arguments[0] == "-c" && int(arguments[1][0]-48) > 0 {
-		for _, carac := range arguments[1] {
-			nb = (nb * 10) + int(carac-48)
+	nbrByte, tabFileName := FindArgs()
+	if len(tabFileName) == 1 {
+		dataFile, err := os.ReadFile(tabFileName[0])
+		check(err)
+		for i := len(dataFile) - nbrByte; i < len(dataFile); i++ {
+			if string(dataFile[i]) != "\n" {
+				fmt.Print(string(dataFile[i]))
+			}
 		}
-		for index, ch := range arguments {
-			if len(arguments) > 3 {
-				if index >= 2 {
-					data, err := os.ReadFile(ch)
-					imin := (len(data) - nb)
-					if err != nil {
-						if index == len(arguments)-1 {
-							fmt.Println(err)
-							break
+	} else {
+		for index, fileName := range tabFileName {
+			dataFile, err := os.ReadFile(tabFileName[index])
+			imin := len(dataFile) - nbrByte
+
+			if check(err) {
+				fmt.Println()
+				fmt.Println("==>", fileName, "<==")
+				if imin > 0 {
+					for i := len(dataFile) - nbrByte; i < len(dataFile); i++ {
+						if string(dataFile[i]) != "\n" {
+							fmt.Println(string(dataFile[i]))
 						}
-						fmt.Println(err)
-						fmt.Println()
-						continue
-					}
-					fmt.Println("==>", ch, "<==")
-					if imin < 0 {
-						for i := 0; i < len(data); i++ {
-							if string(data[i]) != "\n" {
-								fmt.Printf(string(data[i]))
-							}
-						}
-						fmt.Println()
-						os.Exit(1)
-						return
-					} else {
-						for i := imin; i < len(data); i++ {
-							if string(data[i]) != "\n" {
-								fmt.Printf(string(data[i]))
-							}
-						}
-						fmt.Println()
-					}
-				}
-			} else {
-				if index == 2 {
-					data, err := os.ReadFile(ch)
-					imin := (len(data) - nb)
-					if err != nil {
-						if index == len(arguments)-1 {
-							fmt.Println(err)
-							break
-						}
-						fmt.Println(err)
-						fmt.Println()
-						continue
 					}
 					fmt.Println()
-					if imin < 0 {
-						for i := 0; i < len(data); i++ {
-							if string(data[i]) != "\n" {
-								fmt.Printf(string(data[i]))
-							}
+				} else {
+					for i := 0; i < len(dataFile); i++ {
+						if string(dataFile[i]) != "\n" {
+							fmt.Print(string(dataFile[i]))
 						}
-						fmt.Println()
-						os.Exit(1)
-						return
-					} else {
-						for i := imin; i < len(data); i++ {
-							if string(data[i]) != "\n" {
-								fmt.Printf(string(data[i]))
-							}
-						}
-						fmt.Println()
 					}
+					fmt.Println()
+					os.Exit(1)
+					fmt.Println()
 				}
 			}
 		}
 	}
+}
+
+func FindArgs() (int, []string) {
+	var tabFileName []string
+	var nbrByte int
+	arguments := os.Args[1:]
+	if arguments[0] == "-c" && int(arguments[1][0]-48) > 0 {
+		for _, ch := range arguments[1] {
+			nbrByte = (nbrByte * 10) + int(ch-48)
+		}
+		for index, carac := range arguments {
+			if index >= 2 {
+				tabFileName = append(tabFileName, carac)
+			}
+		}
+	}
+	return nbrByte, tabFileName
+}
+
+func check(err error) bool {
+	if err != nil {
+		fmt.Print(err)
+		fmt.Println()
+		return false
+	}
+	return true
 }
